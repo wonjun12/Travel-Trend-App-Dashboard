a4d49069d5072322002a8582a19a5aa37d8a5625
wonjun12 <108748094+wonjun12@users.noreply.github.com>
2023-05-31 16:08:36 +0900

    about 설명 추가

--- app_module/app_about.py
+++ app_module/app_about.py
@@ -5,7 +5,7 @@ def run_about_func(df):
     st.image('image/about_main.png')
     st.header('이번 여행 트렌드는 어디일까?')
     st.text('2019년 ~ 2023년 까지의 데이터를 기준으로 가장 많이 검색된 지역과 검색어를 알아보자!')
-    st.text('')
+    st.text('이전의 데이터를 이용해서 이후의 여행 트렌드를 예측할 수 있도록 하자!')
     st.text('')
     st.subheader('참고 데이터')
     st.dataframe(df)
